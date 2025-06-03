2e27eddc1ec83ffc63532b3fc42aca7b3b3ee096
warreR33 <154948249+warreR33@users.noreply.github.com>
2025-06-02 21:35:22 -0300

    UI and FIX
    Temas de UI adaptado a todos los modulos

--- Assets/Scripts/Player/Equipment/Shields/BaseShield.cs
+++ Assets/Scripts/Player/Equipment/Shields/BaseShield.cs
@@ -7,9 +7,17 @@ public class BaseShield : MonoBehaviour
     public float bonusRechargeRate = 0f;
     public float bonusMaxCharge = 0f;
 
-    public virtual void ApplyTo(HealthSystem healthSystem)
+    protected HealthSystem healthSystem;
+
+    public virtual void ApplyTo(HealthSystem hs)
     {
+        healthSystem = hs;
         healthSystem.SetShieldRechargeRate(healthSystem.shieldRechargeRate + bonusRechargeRate);
         healthSystem.SetMaxShieldCharge(healthSystem.maxShieldCharge + bonusMaxCharge);
     }
+
+    public virtual void UpdateShield() { }
+    public virtual void OnShieldBroken() { }
+    public virtual void OnDamageBlocked() { }
+    public virtual void OnDamageTakenWhileShieldDown() { }
 }
